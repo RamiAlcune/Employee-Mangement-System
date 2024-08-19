65fc017fcc213f36df3965873a6f494ec2b24906
Rami Alcune <ramiabdullrahim@gmail.com>
2024-08-19 22:59:47 +0300

    Start Files

---
+++ DataAccessLayer_EMS/ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer_EMS
+{
+    public class ConnectionString
+    {
+        public string ConnectionStringKeyl = "Server=.;Database=Employee_Management;User Id=sa;Password=sa123456;";
+    }
+}
